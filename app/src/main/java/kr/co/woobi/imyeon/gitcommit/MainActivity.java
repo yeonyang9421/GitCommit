7122627571b6d1d561aeee55c5416c14ec52152c
MinkyungOh <ominomin73@gmail.com>
2018-11-02 16:05:46 +0900

    오민경 commit

--- app/src/main/java/kr/co/woobi/imyeon/gitcommit/MainActivity.java
+++ app/src/main/java/kr/co/woobi/imyeon/gitcommit/MainActivity.java
@@ -2,12 +2,22 @@ package kr.co.woobi.imyeon.gitcommit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((Button)findViewById(R.id.button)).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT).show();
     }
 }
